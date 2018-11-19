f5bf525864032d894a6dc67f661233a6ac24dc4d
Leo Barcellos <contato@leobarcellos.com>
2018-11-18 23:36:27 -0200

    wtf

--- flask_pynamodb_resource/__init__.py
+++ flask_pynamodb_resource/__init__.py
@@ -1,2 +1,2 @@
-from flask_resource import ModelResource, IndexResource, modelresource_factory, indexresource_factory
-__all__ = ['ModelResource', 'IndexResource', 'modelresource_factory', 'indexresource_factory']
+# from flask_resource import ModelResource, IndexResource, modelresource_factory, indexresource_factory
+# __all__ = ['ModelResource', 'IndexResource', 'modelresource_factory', 'indexresource_factory']
